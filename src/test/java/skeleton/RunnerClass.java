ec3843e73051db3fb16e08dbfd8fa7dd0e6853a5
prpise <52442460+prpise@users.noreply.github.com>
2019-07-04 17:19:57 +0530

    Update RunnerClass.java

--- src/test/java/skeleton/RunnerClass.java
+++ src/test/java/skeleton/RunnerClass.java
@@ -8,7 +8,8 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features={"src/test/resources/skeleton/Register.feature","src/test/resources/skeleton/scen1.feature","src/test/resources/skeleton/scenar4.feature","src/test/resources/skeleton/casestudy4.feature"},
 			glue="skeleton",
-			plugin="html:target/htmlreport")
+		 	plugin="json:target/jsonreport.json")
+			//plugin="html:target/htmlreport")
 			//tags={"@smoke"})
 
 	public class RunnerClass {
